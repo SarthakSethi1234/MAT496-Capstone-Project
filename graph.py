6a439f19c60934cb50c79131b433a0010cd6a8d7
Sarthak Sethi <13sarthaksethi@gmail.com>
2025-11-21 16:54:33 +0530

    defining workflow

--- graph.py
+++ graph.py
@@ -53,3 +53,47 @@ def route_chat(state: AgentState):
         return "summarize"
 
     return "end"
+
+# Main Graph
+workflow = StateGraph(AgentState)
+
+workflow.add_node("parse_link", parse_link)
+workflow.add_node("fallback_title_extractor", fallback_title_extractor)
+workflow.add_node("research_subgraph", research_subgraph)
+workflow.add_node("harvest_reviews", harvest_reviews)
+workflow.add_node("generate_report", generate_report)
+workflow.add_node("chat_node", chat_node)
+workflow.add_node("summarize_conversation", summarize_conversation)
+
+tools = [TavilySearchResults(max_results=3)]
+workflow.add_node("tools", ToolNode(tools))
+
+workflow.add_conditional_edges(
+    START,
+    route_start,
+    {"chat_node": "chat_node", "parse_link": "parse_link"}
+)
+
+workflow.add_conditional_edges(
+    "parse_link",
+    check_parser_success,
+    {"success": "research_subgraph", "fail": "fallback_title_extractor"}
+)
+
+workflow.add_edge("fallback_title_extractor", "research_subgraph")
+workflow.add_edge("research_subgraph", "harvest_reviews")
+workflow.add_edge("harvest_reviews", "generate_report")
+workflow.add_edge("generate_report", "chat_node")
+
+workflow.add_conditional_edges(
+    "chat_node",
+    route_chat,
+    {"tools": "tools", "summarize": "summarize_conversation", "end": END}
+)
+
+workflow.add_edge("tools", "chat_node")
+workflow.add_edge("summarize_conversation", END)
+
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
