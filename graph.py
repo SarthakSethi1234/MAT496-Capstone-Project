9391aefecf8bc26a339df90b60ad827cd6f196d9
Sarthak Sethi <13sarthaksethi@gmail.com>
2025-11-21 15:53:21 +0530

    research subgraph

--- graph.py
+++ graph.py
@@ -12,3 +12,19 @@ from nodes import (
 )
 
 load_dotenv()
+
+# Research Subgraph
+research_builder = StateGraph(AgentState)
+research_builder.add_node("researcher_amazon", researcher_amazon)
+research_builder.add_node("researcher_reddit", researcher_reddit)
+research_builder.add_node("researcher_web", researcher_web)
+
+research_builder.add_edge(START, "researcher_amazon")
+research_builder.add_edge(START, "researcher_reddit")
+research_builder.add_edge(START, "researcher_web")
+
+research_builder.add_edge("researcher_amazon", END)
+research_builder.add_edge("researcher_reddit", END)
+research_builder.add_edge("researcher_web", END)
+
+research_subgraph = research_builder.compile()
